707c33b93549a16fc04b14b98831188d344af088
guy-car <guil.crn@gmail.com>
2025-06-02 11:59:21 -0400

    initial commit blank React TS project

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h1>Tic Tac Toe</h1>
+    <div className='gameboard'>
+      <div><p>''</p><p>''</p><p>''</p></div>
+      <div><p>''</p><p>''</p><p>''</p></div>
+      <div><p>''</p><p>''</p><p>''</p></div>
+    </div>
+    </>
+  )
+}
+
+export default App
