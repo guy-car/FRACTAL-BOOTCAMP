1b93631c8d8ebace20e39db8620f8ed0b0533c76
guy-car <guil.crn@gmail.com>
2025-06-02 12:56:49 -0400

    basic UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,27 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
 
+  const initialGame = [
+    [null, null, null],
+    [null, null, null], 
+    [null, null, null]
+  ]
+
+  const [game, setGame] = useState(initialGame)
+
+  const boardEl = game.map(row => row.map(cell => <div className='cell'>test</div>
+  ))
+
+
   return (
     <>
     <h1>Tic Tac Toe</h1>
-    <div className='gameboard'>
-      <div><p>''</p><p>''</p><p>''</p></div>
-      <div><p>''</p><p>''</p><p>''</p></div>
-      <div><p>''</p><p>''</p><p>''</p></div>
+    <div className="game-board">
+      {boardEl}
     </div>
+
     </>
   )
 }
